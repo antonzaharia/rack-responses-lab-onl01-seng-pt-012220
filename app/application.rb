333f1de57b5c7b180d279681b13d9ecfddb3d9fe
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-05 15:12:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,12 +2,9 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    
-    def time
-      Time.now.to_i 
-    end
+
  
-    if time < 12
+    if Time.now.to_i < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
